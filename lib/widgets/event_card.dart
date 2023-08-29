356b2d294f6843feedc827170e6cbc27453716f0
Ashen Prabath <ashenprabath31@gmail.com)>
2023-08-29 15:54:50 +0530

    Fixed EventCard Issues

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -5,137 +5,115 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListView(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Theme.of(context).colorScheme.outline),
-                borderRadius: const BorderRadius.all(Radius.circular(12)),
+     return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              leading: Image.asset('lib/assets/background.png'),
+              title: Text(
+                'Foss Community',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
               ),
-              clipBehavior: Clip.antiAlias,
-              child: Column(
+              subtitle: Row(
                 children: [
-                  ListTile(
-                    leading: Image.asset('lib/assets/background.png'),
-                    title: Text(
-                      'Foss Community',
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface),
-                    ),
-                    subtitle: Row(
-                      children: [
-                        Text(
-                          'Sahan Perera |',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onSurfaceVariant),
-                        ),
-                        Text(
-                          ' 1 day ago',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ],
-                    ),
+                  Text(
+                    'Sahan Perera |',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: Image.asset('lib/assets/media.png'),
+                  Text(
+                    ' 1 day ago',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text("Techie Slueths",
-                            style: Theme.of(context).textTheme.headlineMedium),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "📅 Date : 20th July 2023",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurfaceVariant),
-                            ),
-                            Text(
-                              "⏰ Time : 9.00 am",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurfaceVariant),
-                            ),
-                            Text(
-                              "📍 Venue : FOC-C2-L-110",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurfaceVariant),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      '''It's time to embark on a legendary journey.🕵️
-
-Hey Detectives,today is the day!! Take out your magnifying glass ,Pick up the puzzle and solve the mystery.''',
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color:
-                              Theme.of(context).colorScheme.onSurfaceVariant),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Image.asset('lib/assets/media.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Techie Slueths",
+                      style: Theme.of(context).textTheme.headlineMedium),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text('Free Registration '),
-                        ),
+                      Text(
+                        "📅 Date : 20th July 2023",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: FilledButton(
-                          onPressed: () {},
-                          child: const Text('Register'),
-                        ),
+                      Text(
+                        "⏰ Time : 9.00 am",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
+                      ),
+                      Text(
+                        "📍 Venue : FOC-C2-L-110",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
                       ),
                     ],
                   ),
                 ],
               ),
             ),
-          ),
-        ]),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                '''It's time to embark on a legendary journey.🕵️
+
+Hey Detectives,today is the day!! Take out your magnifying glass ,Pick up the puzzle and solve the mystery.''',
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Free Registration '),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text('Register'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
