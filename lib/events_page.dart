d5daa7085814f7a2edde107665b8924df091327e
Ashen Prabath <ashenprabath31@gmail.com)>
2023-08-29 16:49:17 +0530

    Finished Events>appbar & Bottom nav

--- lib/events_page.dart
+++ lib/events_page.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_material_components_v1/widgets/event_card.dart';
 
-class EventsPage extends StatelessWidget {
+class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
 
   @override
+  State<EventsPage> createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  int currentPageIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        title: Text(
+          "Events",
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge
+              ?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+        ),
+      ),
       body: ListView(
-        children: [
-          EventCard()
-        ]
-      )
-    );
+        children: const [
+          EventCard(),
+          EventCard(),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Theme.of(context).colorScheme.secondaryContainer,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.add),
+            icon: Icon(Icons.add),
+            label: '',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.menu_book),
+            icon: Icon(Icons.menu_book_outlined),
+            label: 'Bookings',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person_2),
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Profile',
+          ),
+        ],
+      ),
+          );
+    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_material_components_v1/events_page.dart';
-import 'package:flutter_material_components_v1/login_page.dart';
 
 void main() {
   runApp(const MyApp());
