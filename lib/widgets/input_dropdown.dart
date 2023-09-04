01ec8539d20facf96eec59cdf406f727bfb7f237
Ashen Prabath <ashenprabath31@gmail.com>
2023-09-04 23:39:03 +0530

    changed some input arguments

--- lib/widgets/input_dropdown.dart
+++ lib/widgets/input_dropdown.dart
@@ -1,36 +1,39 @@
 import 'package:flutter/material.dart';
 
 class InputDropDown extends StatelessWidget {
+  final String? selectedValue;
   final List<String> itemsList;
   final String labelText;
   const InputDropDown(
-      {super.key, required this.itemsList, required this.labelText});
+      {super.key,
+      required this.itemsList,
+      required this.labelText,
+      this.selectedValue});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
-      
-        child: DropdownButtonFormField<String>(
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please fill this field';
-            }
-            return null;
-          },
-          decoration: InputDecoration(
-            labelText: labelText,
-            border: const OutlineInputBorder(),
-          ),
-          items: itemsList.map((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-                      }).toList(),
-          onChanged: (value) {},
+      child: DropdownButtonFormField<String>(
+        value: selectedValue,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please fill this field';
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: const OutlineInputBorder(),
         ),
-      
+        items: itemsList.map((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        onChanged: (value) {},
+      ),
     );
   }
 }
