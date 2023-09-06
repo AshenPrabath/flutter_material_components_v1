1c4da183ecce293ebf106606bbbf8da60dd8afb3
Ashen Prabath <ashenprabath31@gmail.com>
2023-09-07 02:35:40 +0530

    set ticket minicard reusable and dynamic

--- lib/widgets/ticket_minicard.dart
+++ lib/widgets/ticket_minicard.dart
@@ -1,53 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_material_components_v1/Pages/ticket_page.dart';
 
 class TicketMiniCard extends StatelessWidget {
-  const TicketMiniCard({super.key});
+  final String imageLocation;
+  final String eventTitle;
+  final String ticketType;
+  const TicketMiniCard({
+    Key? key,
+    required this.imageLocation,
+    required this.eventTitle,
+    required this.ticketType,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-        border: Border.all(
-          color: Theme.of(context)
-              .colorScheme
-              .outlineVariant, // Change the border color as needed
-          width: 1.0, // Adjust the border width as needed
-        ),
-      ),
-      child: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Green Fiesta 2023",
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface),
-                    ),
-                    Text(
-                      "VIP",
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+    const double borderRadius = 12.0;
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const TicketPage()),
+        );
+      },
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: Border.all(
+            color: Theme.of(context)
+                .colorScheme
+                .outlineVariant, // Change the border color as needed
+            width: 1.0, // Adjust the border width as needed
           ),
-          const Spacer(),
-          ClipRRect(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(12),
-                  bottomRight: Radius.circular(12)),
-              child: Image.asset('lib/assets/media (2).png')),
-        ],
+        ),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        eventTitle,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(
+                                color: Theme.of(context).colorScheme.onSurface),
+                      ),
+                      Text(
+                        ticketType,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.onSurface),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(borderRadius),
+                    bottomRight: Radius.circular(borderRadius)),
+                child: Image.asset(
+                  imageLocation,
+                  height: 80,
+                  width: 80,
+                )),
+          ],
+        ),
       ),
     );
   }
