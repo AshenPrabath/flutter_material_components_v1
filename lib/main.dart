60f1a4cb7cd3f759bdba9925fed2e4178c25db62
Ashen Prabath <ashenprabath31@gmail.com>
2023-09-12 00:27:04 +0530

    set app start with auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_material_components_v1/Pages/login_page.dart';
+import 'package:flutter_material_components_v1/Auth/auth_page.dart';
 import 'package:flutter_material_components_v1/Pages/navigation_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,7 +24,7 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 0, 109, 64)),
         useMaterial3: true,
       ),
-      home: const NavigationPage(),
+      home: const AuthPage(),
     );
   }
 }
