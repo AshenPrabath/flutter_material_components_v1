6a8634b866557b90bff77cd366787b255aad4230
Ashen Prabath <ashenprabath31@gmail.com>
2023-09-14 02:41:03 +0530

    set width editable

--- lib/widgets/custom_filled_button.dart
+++ lib/widgets/custom_filled_button.dart
@@ -5,18 +5,20 @@ class CustomFilledButton extends StatelessWidget {
   final String buttonText;
   final Color? buttonColor;
   final Color? buttonTextColor;
+  final double? width;
   const CustomFilledButton({
     Key? key,
     required this.onPressed,
     required this.buttonText,
     this.buttonColor,
     this.buttonTextColor,
+    this.width,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
+      width: width ?? double.infinity,
       height: 50,
       child: FilledButton(
         style: FilledButton.styleFrom(
